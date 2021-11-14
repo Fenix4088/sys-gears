27e5a5cdeb08ac0062778d4392a7217c153d4380
Fenix4088 <fenix4088@gmail.com>
2021-11-14 19:37:05 +0200

    Start converter logic

--- src/app/measure-converter(task-1)/app.ts
+++ src/app/measure-converter(task-1)/app.ts
@@ -1,3 +1,33 @@
 import config from './config.json';
+import input from './input.json';
+import extendConfig from "./extend_config.json";
 
-console.log(config);
+import {TConfig, IInputData} from "./types";
+
+
+
+class MeasureConverter {
+      private config: TConfig;
+      private input: IInputData;
+      private extendConfig?: TConfig; 
+
+      constructor(config: TConfig, input: IInputData, extendConfig?: TConfig) {
+            this.config = config;
+            this.input = input;
+            this.extendConfig = extendConfig;           
+      }
+
+      public init() {
+            const {extendConfiguration, config, extendConfig} = this;
+            extendConfiguration(config, extendConfig);
+
+      }
+
+      private extendConfiguration = (config: TConfig, extendConfig?: TConfig) => {
+            if(!extendConfig) return;
+            
+      }
+}
+
+const converter = new MeasureConverter(config, input as IInputData, extendConfig);
+converter.init();
